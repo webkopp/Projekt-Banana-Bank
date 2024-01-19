00ed6d317b222229daf9bcf062616691f0f481e3
webkopp <nakopps@googlemail.com>
2024-01-19 14:57:51 +0100

    compo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-
+import { useState } from 'react'
 import './App.css'
+import Kontostand from './assets/components/kontostand/Kontostand'
+import Form from './assets/components/form/Form'
 
 function App() {
 
+  const [obj, setObj] = useState({
+    saldo: ""
+  })
+
   return (
     <>
-      
+      <div>
+        <h1>Banana Bank</h1>
+
+        <Form
+          setObj={setObj}
+          obj={obj}
+        />
+        <Kontostand
+          KontoObj={KontoObj}
+          setKontoObj={setKontoObj}
+        />
+        </div>
+
     </>
   )
 }
